b86aa833c6acb265bf581decc98975f9e2c8b21a
Administrator <dash@clearpathrobotics.com>
2017-05-11 12:49:35 -0400

    Changed license

--- dual_ur5_teleop_general/src/dual_ur5_teleop_general.py
+++ dual_ur5_teleop_general/src/dual_ur5_teleop_general.py
@@ -2,7 +2,7 @@
 
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2013, Willow Garage, Inc.
+# Copyright (c) 2017, Clearpath Robotics, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -15,7 +15,7 @@
 #    copyright notice, this list of conditions and the following
 #    disclaimer in the documentation and/or other materials provided
 #    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
+#  * Neither the name of Clearpath Robotics, Inc. nor the names of its
 #    contributors may be used to endorse or promote products derived
 #    from this software without specific prior written permission.
 #
@@ -32,7 +32,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-# Author: Ioan Sucan
+# Author: Devon Ash
 
 import sys
 import rospy
