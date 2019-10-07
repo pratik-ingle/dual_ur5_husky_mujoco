3911f2e3a49b2321ea79faee228f90e3ae5e84d8
Cong Wang <wangcongrobot@gmail.com>
2019-10-07 16:48:48 +0100

    get dual ur5 husky mujoco model

--- husky_description/urdf/generate_dual_arm_husky_urdf.sh
+++ husky_description/urdf/generate_dual_arm_husky_urdf.sh
@@ -3,3 +3,5 @@
 
 #!/bin/sh
 rosrun xacro xacro --inorder dual_arm_husky_mujoco.urdf.xacro laser_enabled:=true ur5_enabled:=true kinect_enabled:=true dual_ur5_enabled:=true robotiq_grippers_enabled:=true > dual_arm_husky_mujoco.urdf
+
+check_urdf dual_arm_husky_mujoco.urdf
